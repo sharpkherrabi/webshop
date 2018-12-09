04af6b3c14499ae125531312178e902b89b8bde5
Tim Endruszeit <tim.endruszeit@gmail.com>
2018-12-09 20:44:22 +0100

    added files for service and model

--- client/client/src/app/app-routing.module.ts
+++ client/client/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'webshop',
     pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    component: ProductListComponent
   }
 ];
 
