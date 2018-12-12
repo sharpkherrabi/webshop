ce58b8cae70afd68a2693d495f3d39271c2ea30a
Tim Endruszeit <tim.endruszeit@gmail.com>
2018-12-12 17:11:13 +0100

    basic layout, false scaling

--- client/client/src/app/pages/product-list/product-list.component.ts
+++ client/client/src/app/pages/product-list/product-list.component.ts
@@ -15,17 +15,22 @@ export class ProductListComponent implements OnInit {
 
   constructor() {
     if (this.mode === 'large') {
-      this.columns = 8;
+      this.columns = 4;
     } else {
-      this.columns = 2;
+      this.columns = 1;
     }
   }
 
   ngOnInit() {
-     const p1: Product = {name: 'eins'};
-     const p2: Product = {name: 'zwei'};
-     const p3: Product = {name: 'drei'};
-     this.products = [p1, p2,  p3];
+     const p1: Product = {name: '1'};
+     const p2: Product = {name: '2'};
+     const p3: Product = {name: '3'};
+     const p4: Product = {name: '4'};
+     const p5: Product = {name: '5'};
+     const p6: Product = {name: '6'};
+     const p7: Product = {name: '7'};
+     const p8: Product = {name: '8'};
+     this.products = [p1, p2,  p3, p4, p5, p6, p7, p8];
   }
 
 }
