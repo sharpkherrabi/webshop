6a1eb03892d1bb2d26b37a9640f2494bab5256f4
Sebastian Subici <sebastian.subici@ion2s.com>
2018-12-17 11:13:26 +0100

    Added Product Item Detail

--- client/client/src/app/pages/product-item-detail/product-item-detail.component.ts
+++ client/client/src/app/pages/product-item-detail/product-item-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../../models/product";
+import {ShopService} from "../../services/shop.service";
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-product-item-detail',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item-detail.component.css']
 })
 export class ProductItemDetailComponent implements OnInit {
+  products: Product[] = [];
+  splicedData: Product[] = [];
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
 
-  constructor() { }
+
+  constructor(private shopService: ShopService, private router: Router) { }
 
   ngOnInit() {
-  }
+    // for develop
+    const p1: Product = {name: '1',description:'Diese Ware ist toll'};
 
+    this.products = [p1];
+    this.splicedData = this.products.slice(((0 + 1) - 1) * this.pageSize).slice(0, this.pageSize);
+
+  }
+  onBackClicked() {
+    this.router.navigate(['/products']);
+  }
 }
