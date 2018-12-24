ebfbcf0b6be9c0498fa7d7a88cc29c6ed3ce9eaf
Tim Endruszeit <tim.endruszeit@gmail.com>
2018-12-24 17:39:44 +0100

    tabs products/orders, first layout elements

--- client/client/src/app/app-routing.module.ts
+++ client/client/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './pages/product-list/product-list.component';
-import { ProductItemDetailComponent} from "./pages/product-item-detail/product-item-detail.component";
+import { ProductItemDetailComponent} from './pages/product-item-detail/product-item-detail.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'productdetail',
     component: ProductItemDetailComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
 
