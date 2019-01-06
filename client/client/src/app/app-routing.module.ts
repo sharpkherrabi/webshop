78c4cb7b48143986385eaad5d65e4bdf300b6b71
Sebastian <sebastian.subici@stud.h-da.de>
2019-01-06 14:28:54 +0100

    Add Cart Site

--- client/client/src/app/app-routing.module.ts
+++ client/client/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     component: DashboardComponent
   },
   {
-    path: 'shoppingCart',
+    path: 'shoppingcart',
     component: CartComponent
   }
 ];
--- client/client/src/app/pages/cart/cart.component.ts
+++ client/client/src/app/pages/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../../models/product";
+import {ShopService} from "../../services/shop.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  splicedDataProducts: Product[] = []; // data to show on one page
+  splicedData: Product[] = [];
+
+  // MatPaginator Inputs
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  constructor(private shopService: ShopService, private router: Router) { }
 
   ngOnInit() {
+    // for develop
+    const p1: Product = {
+      name: '1',
+      description: 'texttexttext',
+      quantity: 10,
+      unitPrice: 9999,
+      mass: 50,
+      image: 'https://images.pexels.com/photos/776656/pexels-photo-776656.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'};
+
+    this.products = [p1];
+    this.splicedData = this.products.slice(((0 + 1) - 1) * this.pageSize).slice(0, this.pageSize);
   }
 
 
