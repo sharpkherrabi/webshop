52c60883c266f65bac9de3b710cd74a8ee3be6b0
Tim Endruszeit <tim.endruszeit@gmail.com>
2019-01-13 18:59:20 +0100

    cart: sum of all products in cart

--- client/client/src/app/pages/cart/cart.component.ts
+++ client/client/src/app/pages/cart/cart.component.ts
@@ -14,20 +14,25 @@ export class CartComponent implements OnInit {
 
   cart: Product[];
   cartCount: number = 0;
+  sum: number = 0;
 
   constructor(private shopService: ShopService, private router: Router, private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
-
     this.localStorageService.getLocalStorage().then((products) => {
       this.cart = products;
       this.cartCount = products.length;
+
+      for(let i=0;i<this.cartCount;i++) {
+        this.sum = this.sum + this.cart[i].unitPrice;
+      }
     });
   }
 
   removeProduct(product: Product){
       for(let i=0;i<this.cart.length;i++) {
           if(this.cart[i]._id === product._id){
+              this.sum = this.sum - this.cart[i].unitPrice;
               this.cart.splice(i,1);
               break;
           }
