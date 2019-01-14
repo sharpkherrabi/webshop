156036db6e0c6bd13e7562d3cae1668825cb658d
Marina Reichert <shil7en@gmail.com>
2019-01-14 17:35:20 +0100

    merge new order model

--- client/client/src/app/models/order.ts
+++ client/client/src/app/models/order.ts
@@ -1,22 +1,6 @@
 import {Product} from "./product";
 import { AriaDescriber } from '@angular/cdk/a11y';
 
-<<<<<<< HEAD
-export class Order {
-
-  //Person
-  firstname: string;
-  lastname: string;
-  email: string;
-  phoneNumber: string;
-
-  //Adress:
-  street: string;
-  city: string;
-  state: string; // or province
-  postalCode: number;
-  country: string;
-=======
 export class Orderer{
   firstname: string;
   lastname: string;
@@ -31,7 +15,7 @@ export class Address{
     Object.assign(this, values);
   }
 }
->>>>>>> feature/checkout
+
 
 export class ProductInfo{
   id: string;
