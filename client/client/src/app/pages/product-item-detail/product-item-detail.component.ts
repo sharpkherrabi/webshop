07590142c4fb35b240d18a1ea6f40f268be05d9b
Marouane Kherrabi <marouane.kherrabi@stud.h-da.de>
2019-02-13 21:29:59 +0100

    correct cart in detail page

--- client/client/src/app/pages/product-item-detail/product-item-detail.component.ts
+++ client/client/src/app/pages/product-item-detail/product-item-detail.component.ts
@@ -3,7 +3,7 @@ import { Product } from '../../models/product';
 import { ShopService } from '../../services/shop.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LocalStorageService } from "../../services/local-storage.service";
-import {MatSnackBar} from '@angular/material';
+import { MatSnackBar } from '@angular/material';
 import * as _ from 'lodash';
 @Component({
 	selector: 'app-product-item-detail',
@@ -27,15 +27,20 @@ export class ProductItemDetailComponent implements OnInit {
 		private snackBar: MatSnackBar) { }
 
 	ngOnInit() {
-		this.route.queryParams.subscribe((params) => {
+		this.route.queryParams.subscribe(async (params) => {
 			const id = params['productId'];
 			if (id === 'new') {
 				//this.mode = 'new';
 				this.product = new Product();
 			} else {
-				this.shopService.getProduct(id).then((result) => {
-					this.product = result.products[0];
-				});
+				let localStorageProducts = await this.localStorageService.getLocalStorage();
+				if (!_.some(localStorageProducts, { _id: id })) {
+					this.shopService.getProduct(id).then((result) => {
+						this.product = result.products[0];
+					});
+				} else {
+					this.product = _.find(localStorageProducts, ['_id', id]);
+				}
 			}
 		});
 		if (this.localStorageService.isLocalStorageSet())
